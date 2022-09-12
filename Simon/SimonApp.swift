62151c198177d9145f16ad504fc333053ec04c22
VictorGun <Thunderboomboom7@gmail.com>
2022-09-12 09:49:51 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Victor Gunderson on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
