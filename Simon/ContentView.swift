f0b316dcc192872a53091948b6d513cf3ad02971
VictorGun <Thunderboomboom7@gmail.com>
2022-09-15 09:33:23 -0500

    made color game but no ending or correct or wrong

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
     @State private var flash = [false, false, false, false]
+    @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    @State private var index = 0
+    @State private var sequence = [0,1,2,3]
     var body: some View {
         VStack {
             Text("Simon")
@@ -17,21 +20,46 @@ struct ContentView: View {
                 .fontWeight(.bold)
                 .multilineTextAlignment(.center)
                 .preferredColorScheme(.dark)
+                .onReceive(timer) { _ in
+                    if index < sequence.count {
+                        flashColorDisplay(index: sequence[index])
+                        index += 1
+                    }
+                }
             Spacer()
             
             HStack {
                 colorDisplay[0]
                     .opacity(flash[0] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 0)
+                    }
                 colorDisplay[1]
                     .opacity(flash[1] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 1)
+                    }
             }
             HStack {
                 colorDisplay[2]
                     .opacity(flash[2] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 2)
+                    }
                 colorDisplay[3]
                     .opacity(flash[3] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 3)
+                    }
             }
             Spacer()
+        }
+        
+    }
+    func flashColorDisplay(index: Int) {
+        flash[index].toggle()
+        withAnimation(.easeInOut(duration: 0.5)) {
+            flash[index].toggle()
         }
     }
 }
